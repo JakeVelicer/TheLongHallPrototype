bac717f24235c7a3119c716c106244fa9950f15f
Fr4k26 <afrak@mail.bradley.edu>
2019-09-14 17:33:22 -0500

    Repeating Hallway Updated

--- The Long Hall/Assets/Scripts/ReloadScene.cs
+++ The Long Hall/Assets/Scripts/ReloadScene.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class ReloadScene : MonoBehaviour
 {
+    public Transform teleportPoint;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene("SampleScene");
+        player.position = teleportPoint.position;
+        player.rotation = teleportPoint.rotation;
     }
 }
