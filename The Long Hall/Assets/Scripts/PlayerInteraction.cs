ceda061f8260a9b5442c9b9bc87a6beb27527569
Jake Velicer <007velicer@gmail.com>
2019-09-19 14:25:10 -0500

    Can pickup objects kind of

---
+++ The Long Hall/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float speed = 10;
+    public bool canHold = true;
+    public GameObject bodyPart;
+    public Transform guide;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+        }//mause If
+
+        if (!canHold && bodyPart)
+            bodyPart.transform.position = guide.position;
+        
+    }//update
+
+    //We can use trigger or Collision
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "BodyPart")
+        {
+            Debug.Log("BodyPart");
+            if (!bodyPart) // if we don't have anything holding
+                bodyPart = col.gameObject;
+
+            if (!canHold)
+                throw_drop();
+            else
+                Pickup();
+        }
+    }
+
+    //We can use trigger or Collision
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "BodyPart")
+        {
+            if (canHold)
+                bodyPart = null;
+        }
+    }
+
+
+    private void Pickup()
+    {
+        if (!bodyPart)
+            return;
+
+        //We set the object parent to our guide empty object.
+        bodyPart.transform.SetParent(guide);
+
+        //Set gravity to false while holding it
+        bodyPart.GetComponent<Rigidbody>().useGravity = false;
+        bodyPart.GetComponent<Rigidbody>().isKinematic = true;
+
+        //we apply the same rotation our main object (Camera) has.
+        bodyPart.transform.localRotation = transform.rotation;
+        //We re-position the ball on our guide object 
+        bodyPart.transform.position = guide.position;
+
+        canHold = false;
+    }
+
+    private void throw_drop()
+    {
+        if (!bodyPart)
+            return;
+
+        //Set our Gravity to true again.
+        bodyPart.GetComponent<Rigidbody>().useGravity = true;
+        bodyPart.GetComponent<Rigidbody>().isKinematic = false;
+        // we don't have anything to do with our ball field anymore
+        bodyPart = null; 
+        //Apply velocity on throwing
+        guide.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+        //Unparent our ball
+        guide.GetChild(0).parent = null;
+        canHold = true;
+    }
+}
