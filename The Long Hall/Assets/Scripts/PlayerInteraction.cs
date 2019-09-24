44ee075c61fdce1a81e4a4146ec8f1caaf16794d
Jake Velicer <007velicer@gmail.com>
2019-09-24 15:45:42 -0500

    Player interaction almost done

--- The Long Hall/Assets/Scripts/PlayerInteraction.cs
+++ The Long Hall/Assets/Scripts/PlayerInteraction.cs
@@ -60,8 +60,8 @@ public class PlayerInteraction : MonoBehaviour
             return;
 
         //We set the object parent to our guide empty object.
+        puzzlePiece.transform.parent = null;
         puzzlePiece.transform.SetParent(guide);
-        
         
         //Set gravity to false while holding it
         puzzlePiece.GetComponent<Rigidbody>().useGravity = false;
