2fe52aceccd1ae7e7b593b7a035bf31d8c793fd2
Alex Cook <alex_cook@Alexs-MacBook-Pro-2.local>
2019-09-26 16:12:17 -0500

    Walking
    Sounds now while you walk

--- The Long Hall/Assets/Scripts/PlayerMovement.cs
+++ The Long Hall/Assets/Scripts/PlayerMovement.cs
@@ -11,11 +11,16 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 RightMovement;
     private Rigidbody rb;
     private Vector3 velocity;
+
+   public AudioSource source;
+  //  public AudioClip footStep;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
+        InvokeRepeating("PlaySound", 0.0f, 0.5f);
     }
 
     // Update is called once per frame
@@ -23,6 +28,7 @@ public class PlayerMovement : MonoBehaviour
     {
         horizInput = Input.GetAxis("Horizontal");
         vertInput = Input.GetAxis("Vertical");
+
     }
     private void FixedUpdate()
     {
@@ -36,5 +42,18 @@ public class PlayerMovement : MonoBehaviour
         // Moves the players rigidbody by the velocity variable and multiplies it by Time.deltaTime
         rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
 
+        //checks to see if the player is moving
+       
+    }
+    void PlaySound()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            //plays the footstep at different volume ranges
+            source.volume = Random.Range(0.8f, 1f);
+            source.pitch = Random.Range(0.8f, 1f);
+            //plays the audio
+            source.Play();
+        }
     }
 }
