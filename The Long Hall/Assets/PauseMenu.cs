491e8bde7a0abd36edaa11ac0e7c0e96ed468f0d
Alex Cook <alex_cook@Alexs-MacBook-Pro-2.local>
2019-09-29 18:12:28 -0500

    Pause menu
    We have a pause menu with how to play

---
+++ The Long Hall/Assets/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUi;
+
+    public Scene GameScene;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+
+    }
+    public void restartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void HowToPlay()
+    {
+
+    }
+}
